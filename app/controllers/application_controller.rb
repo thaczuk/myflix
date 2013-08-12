262e9923996e6a2a290d778dbd2082d06bde0b89
Gregory Thaczuk <thaczuk@gmail.com>
2013-08-12 07:18:58 -0600

    Add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    # Check database for @current_user, if nil then find user, else return @current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    unless logged_in?
+      flash[:error] = "Access for members only. Please sign in."
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_filter :require_user
+
   def show
     @category = Category.find params[:id]
   end
