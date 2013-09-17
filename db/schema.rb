f027560fea7fe36fb123592c5f51fbe30f0ac041
Gregory Thaczuk <thaczuk@gmail.com>
2013-09-17 12:07:26 -0600

    Invite users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130915164027) do
+ActiveRecord::Schema.define(:version => 20130917155017) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20130915164027) do
     t.text     "message"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "token"
   end
 
   create_table "queue_videos", :force => true do |t|
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -30,6 +30,29 @@ describe "POST #create" do
         post :create, user: {full_name: 'Greg Thaczuk', password: 'pass', email: "greg@example.com"}
         flash[:success].should == "You are registered. Please sign in."
       end
+
+      it "makes the user follow the invitor" do
+        alice = Fabricate(:user)
+        invitation = Fabricate(:invitation, inviter: alice, recipient_email: 'joe@example.com')
+        post :create, user: {email: "joe@example.com", password: "password", full_name: "joe doe"}, invitation_token: invitation.token
+        joe = User.where(email: "joe@example.com").first
+        expect(joe.follows?(alice)).to be_true
+      end
+
+      it "makes the inviter follow the user" do
+        alice = Fabricate(:user)
+        invitation = Fabricate(:invitation, inviter: alice, recipient_email: 'joe@example.com')
+        post :create, user: {email: "joe@example.com", password: "password", full_name: "joe doe"}, invitation_token: invitation.token
+        joe = User.where(email: "joe@example.com").first
+        expect(alice.follows?(joe)).to be_true
+      end
+
+      it "expires the invitation upon acceptance" do
+        alice = Fabricate(:user)
+        invitation = Fabricate(:invitation, inviter: alice, recipient_email: 'joe@example.com')
+        post :create, user: {email: "joe@example.com", password: "password", full_name: "joe doe"}, invitation_token: invitation.token
+        expect(Invitation.first.token).to be_nil
+      end
     end
 
     context "email sending" do
@@ -72,6 +95,31 @@ describe "POST #create" do
       alice = Fabricate(:user)
       get :show, id: alice.id
       expect(assigns(:user)).to eq(alice)
+    end
+  end
+
+  describe "GET new_with_invitation_token" do
+    it "renders the :new view template" do
+      invitation = Fabricate(:invitation)
+      get :new_with_invitation_token, token: invitation.token
+      expect(response).to render_template :new
+    end
+
+    it "sets @user with recipient's email" do
+      invitation = Fabricate(:invitation)
+      get :new_with_invitation_token, token: invitation.token
+      expect(assigns(:user).email).to eq(invitation.recipient_email)
+    end
+
+    it "sets @invitation_token" do
+      invitation = Fabricate(:invitation)
+      get :new_with_invitation_token, token: invitation.token
+      expect(assigns(:invitation_token)).to eq(invitation.token)
+    end
+
+    it "redirects to expired token page for invalid tokens" do
+      get :new_with_invitation_token, token: 'asfdfas'
+      expect(response).to redirect_to expired_token_path
     end
   end
 end
