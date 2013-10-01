1d6d902c2b695baba1afcc2fd9a39872b6c02c17
Gregory Thaczuk <thaczuk@gmail.com>
2013-10-01 14:07:03 -0600

    Changed carrrierwave variable names

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
       :provider                         => 'AWS',
-      :aws_access_key_id         => ENV['S3_KEY'],
-      :aws_secret_access_key   => ENV['S3_SECRET']
+      :aws_access_key_id         => ENV['S3_ACCESS_KEY_ID'],
+      :aws_secret_access_key   => ENV['S3_SECRET_ACCESS_KEY']
     }
     config.fog_directory          = ENV['S3_BUCKET_NAME']
   else
